07c1770f92b59e3da013ab211db52c139efbe6c2
moveSecondSand <aleksandra.ewa.kolacz@gmail.com>
2020-01-08 11:40:37 +0100

    js, index

---
+++ static/js/script.js
@@ -0,0 +1,54 @@
+window.onload = function() {
+    dragula([document.getElementById('left'), document.getElementById('right')]);
+};
+
+const listOfAllMembers = [];
+
+const form = document.querySelector('form');
+const ol = document.querySelector('ol');
+const numberOfMembers = document.querySelector('h4 span');
+const listName = document.getElementsByClassName('memberName');
+const input = document.querySelector('input');
+
+const removeName = (e) => {
+    const index = e.target.parentNode.dataset.key;
+    listOfAllMembers.splice(index, 1);
+    numberOfMembers.textContent = listOfAllMembers.length;
+    renderList();
+}
+
+const addTask = (e) => {
+    e.preventDefault();
+    const memberName = input.value;
+    if (memberName === "") return alert('Write name!');
+    const newName = document.createElement('li');
+    newName.className = 'memberName';
+    newName.innerHTML = memberName + "<button>Remove</button>";
+    listOfAllMembers.push(newName);
+    renderList();
+    ol.appendChild(newName);
+    input.value = '';
+    numberOfMembers.textContent = listName.length;
+    newName.querySelector('button').addEventListener('click', removeName);
+}
+
+const renderList = () => {
+    ol.textContent = '';
+    listOfAllMembers.forEach((singleMember, index) => {
+        singleMember.dataset.key = index;
+        ol.appendChild(singleMember);
+    });
+}
+
+form.addEventListener('submit', addTask)
+
+const buttonGenerator = document.querySelector('#generate');
+const randomizer = document.querySelector('div.randomize')
+
+const groupGenerator = () => {
+    const randomIndex = Math.floor(Math.random() * listOfAllMembers.length);
+
+    randomizer.textContent = `Groups list ${listOfAllMembers[randomIndex].textContent}`;
+}
+
+buttonGenerator.addEventListener('click', groupGenerator);
