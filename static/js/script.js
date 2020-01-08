cce36a0113a38c0082563ab1990db3910b00f275
Harnet69 <harnet69@gmail.com>
2020-01-08 21:02:30 +0100

    Adding members works

--- static/js/script.js
+++ static/js/script.js
@@ -2,69 +2,61 @@ window.onload = function() {
     dragula([document.getElementById('left'), document.getElementById('right')]);
 };
 
-const listOfAllMembers = [];
-
-const form = document.querySelector('form');
-const ol = document.querySelector('ol');
-const numberOfMembers = document.querySelector('h4 span');
-const listName = document.getElementsByClassName('memberName');
-const input = document.querySelector('input');
-const genButton = document.getElementById('generate');
-
-const removeName = (e) => {
-    const index = e.target.parentNode.dataset.key;
-    listOfAllMembers.splice(index, 1);
-    numberOfMembers.textContent = listOfAllMembers.length;
-    renderList();
-};
-
-const clickedDelButton = (e) => {
+// slowly disappear element
+function elemDisapp(genButton){
     let opacity =1;
     let timeOut = setInterval(function () {
         genButton.style.opacity = opacity;
-        opacity -= 0.03;
+        opacity -= 0.005;
         if(opacity <= 0){
             clearInterval(timeOut);
             console.log('STOP');
         }
-    },10);
+    },1);
 
-};
-
-
-const addTask = (e) => {
-    e.preventDefault();
-    const memberName = input.value;
-    if (memberName === "") return alert('Write name!');
-    const newName = document.createElement('li');
-    newName.className = 'memberName';
-    newName.innerHTML = memberName + "<button>Remove</button>";
-    listOfAllMembers.push(newName);
-    renderList();
-    ol.appendChild(newName);
-    input.value = '';
-    numberOfMembers.textContent = listName.length;
-    newName.querySelector('button').addEventListener('click', removeName);
 }
 
-const renderList = () => {
-    ol.textContent = '';
-    listOfAllMembers.forEach((singleMember, index) => {
-        singleMember.dataset.key = index;
-        ol.appendChild(singleMember);
+// add event click to a generate button
+function genButton() {
+    const genButton = document.getElementById('generate');
+    genButton.addEventListener('click', function () {
+        elemDisapp(genButton);
     });
 }
 
-form.addEventListener('submit', addTask);
-genButton.addEventListener('click', clickedDelButton);
-
-const buttonGenerator = document.querySelector('#generate');
-const randomizer = document.querySelector('div.randomize')
-
-const groupGenerator = () => {
-    const randomIndex = Math.floor(Math.random() * listOfAllMembers.length);
-
-    randomizer.textContent = `Groups list ${listOfAllMembers[randomIndex].textContent}`;
+// add action to input field
+function addMemberInputField() {
+    let addMemberInput = document.querySelector('#add_member_input');
+    addMemberInput.addEventListener('keyup', function () {
+         if (event.keyCode === 13) { // if 'Enter' was pressed on a input field
+            addMemberRecord(addMemberInput.value);
+         }
+    });
 }
 
-buttonGenerator.addEventListener('click', groupGenerator);
+// add member with button Add
+function addMemberButton() {
+    let addMemberButton = document.querySelector('#add_btn');
+    addMemberButton.addEventListener('click', function () {
+        let memberNameInputField = document.querySelector('#add_member_input');
+        addMemberRecord(memberNameInputField.value);
+    });
+}
+
+// create a new user information div
+function addMemberRecord(memberName) {
+    let memberDispHTML = "<label id='member_name'>"+memberName+"</label><img id='delButton' src=\"/static/img/del_btn.png\" alt=\"\">";
+    let usersUl = document.getElementById('listOfMembers');
+    let usersli = document.createElement('div');
+    usersli.classList.add('member_name');
+    usersli.innerHTML = memberDispHTML;
+    usersUl.appendChild(usersli);
+}
+
+function main() {
+    genButton();
+    addMemberInputField();
+    addMemberButton();
+}
+
+main();
