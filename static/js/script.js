6ee85e1f0a1c8f1da8fbe62337643396711e6e2a
Harnet69 <harnet69@gmail.com>
2020-01-09 09:02:16 +0100

    Added a clearing field functionality

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,8 @@ window.onload = function() {
     dragula([document.getElementById('left'), document.getElementById('right')]);
 };
 
+let memberNum = 0;
+
 // slowly disappear element
 function elemDisapp(genButton){
     let opacity =1;
@@ -28,39 +30,56 @@ function genButton() {
 }
 
 // add action to input field
-function addMemberInputField(memberNum) {
+function addMemberInputField() {
     let addMemberInput = document.querySelector('#add_member_input');
     addMemberInput.addEventListener('keyup', function () {
          if (event.keyCode === 13) { // if 'Enter' was pressed on a input field
-            addMemberRecord(addMemberInput.value, memberNum);
+            addMemberRecord(addMemberInput.value);
          }
     });
 }
 
-// add member with button Add
-function addMemberButton(memberNum) {
+// add member with button add
+function addMemberButton() {
     let addMemberButton = document.querySelector('#add_btn');
     addMemberButton.addEventListener('click', function () {
         let memberNameInputField = document.querySelector('#add_member_input');
-        addMemberRecord(memberNameInputField.value, memberNum);
+        addMemberRecord(memberNameInputField.value);
     });
 }
 
 // create a new user information div
-function addMemberRecord(memberName, memberNum) {
-    let memberDispHTML = "<label id='member"+memberNum+"_name'>"+memberName+"</label><img id='del"+memberNum+"Button' src=\"/static/img/del_btn.png\" alt=\"\">";
+function addMemberRecord(memberName) {
+    let memberDispHTML = "<label id='member"+memberNum+"_name'>"+memberName+"</label><img class='del_button' id='del"+memberNum+"Button' src=\"/static/img/del_btn.png\" alt=\"\">";
     let usersUl = document.getElementById('listOfMembers');
     let usersli = document.createElement('div');
     usersli.classList.add('member_name');
     usersli.innerHTML = memberDispHTML;
     usersUl.appendChild(usersli);
+    memberNum++;
+    clearInputField();
+    delMember();
 }
 
+// clear input field after adding a record
+function clearInputField() {
+    let addMemberInput = document.querySelector('#add_member_input');
+    addMemberInput.value = '';
+}
+
+// add listener to delete member
+function delMember() {
+    let delButton = document.getElementsByClassName('del_button');
+    delButton[0].addEventListener('click', function () {
+        console.log(delButton[0].getAttribute('id'));
+    });
+}
+
+
 function main() {
-    var memberNum = 0;
     genButton();
-    addMemberInputField(memberNum);
-    addMemberButton(memberNum);
+    addMemberInputField();
+    addMemberButton();
 }
 
 main();
